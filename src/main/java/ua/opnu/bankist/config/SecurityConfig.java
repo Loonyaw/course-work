5971a92334048aa6b55ef32a789ade8bce02e97e
Loonyaw <kurtcobainuarethebest@gmail.com>
2024-05-27 01:19:17 +0300

    added spring security

--- src/main/java/ua/opnu/bankist/config/SecurityConfig.java
+++ src/main/java/ua/opnu/bankist/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import ua.opnu.bankist.service.CustomUserDetailsService;
 
 import java.util.List;
@@ -32,7 +37,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(request -> {
-                    var corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
                     corsConfiguration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
                     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfiguration.setAllowedHeaders(List.of("*"));
@@ -40,27 +45,48 @@ public class SecurityConfig {
                     return corsConfiguration;
                 }))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/users/exists").permitAll()
-                        .requestMatchers("/api/users/login").permitAll()
-                        .requestMatchers("/api/users/**").permitAll()
-                        .requestMatchers("/api/cards/**").permitAll()
-                        .requestMatchers("/api/loans/**").permitAll()
-                        .requestMatchers("/api/transactions/**").permitAll()
+                        .requestMatchers("/api/users/validateCredentials", "/api/users/exists", "/api/users").permitAll()
+                        .requestMatchers("/api/cards/**", "/api/loans/**", "/api/transactions/**").permitAll()
+                        .anyRequest().permitAll()
                 )
                 .formLogin(form -> form
-                        .loginPage("/src/pages/login/login.html").permitAll()
+                        .loginPage("http://127.0.0.1:5500/src/pages/login/login.html").permitAll()
                         .loginProcessingUrl("/perform_login")
-                        .defaultSuccessUrl("/src/pages/dashboard/dashboard.html", true)
-                        .failureUrl("/src/pages/login/login.html?error=true")
+                        .defaultSuccessUrl("http://127.0.0.1:5500/src/pages/dashboard/dashboard.html", true)
+                        .failureUrl("http://127.0.0.1:5500/src/pages/login/login.html?error=true")
                 )
                 .logout(logout -> logout
                         .logoutUrl("/perform_logout")
-                        .logoutSuccessUrl("/src/pages/login/login.html").permitAll()
+                        .logoutSuccessUrl("http://127.0.0.1:5500/src/pages/login/login.html").permitAll()
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                );
+                )
+                .sessionManagement(session -> session
+                        .invalidSessionUrl("http://127.0.0.1:5500/src/pages/login/login.html")
+                        .maximumSessions(1)
+                )
+                .requestCache(requestCache -> requestCache.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public HttpFirewall allowSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://127.0.0.1:5500");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
